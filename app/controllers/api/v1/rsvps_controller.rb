f0e963870e44444609f99ce13ede9cb5bdf04c72
Galen <galen.h.white@gmail.com>
2018-06-07 16:04:23 -0400

    typo

--- app/controllers/api/v1/rsvps_controller.rb
+++ app/controllers/api/v1/rsvps_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RsvpsController < ApplicationController
-  protect_from_forgery unles: -> { request.format.json? }
+  protect_from_forgery unless: -> { request.format.json? }
   def index
     render json: RSVP.all
   end
