a0ee9f4aeb228a258f9498b9bf6c50eab365e6c8
Galen <galen.h.white@gmail.com>
2018-06-17 18:24:50 -0400

    devise still won't access current_user in api_controller

--- app/controllers/api/v1/invites_controller.rb
+++ app/controllers/api/v1/invites_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::InvitesController < ApplicationController
+  # skip_before_action :verify_authenticity_token
+  # before_action :authenticate_user!
   protect_from_forgery unless: -> { request.format.json? }
 
   def find
