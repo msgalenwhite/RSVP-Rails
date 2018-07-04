d86e49e12623e502d2b8e8147685a8513c4241b5
Galen <galen.h.white@gmail.com>
2018-07-04 10:40:23 -0400

    added summary job

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec puma -C config/puma.rb
+summary_worker: bundle exec sidekiq -c 1 
--- app/mailers/summary_mailer.rb
+++ app/mailers/summary_mailer.rb
@@ -1,10 +1,10 @@
 class SummaryMailer < ApplicationMailer
-  def send_out(user)
-    @rsvps = format_rsvps
+  def send_out(email)
+    @sorted_responses = format_rsvps
 
     mail(
-      to: ENV["GALEN_EMAIL"],
-      subject: "Thank you for RSVP-ing to Galen and Chris's Wedding",
+      to: email,
+      subject: "Your Daily RSVP Summary",
       template_path: 'summary_mailer',
       template_name: 'send_out'
     )
@@ -17,18 +17,24 @@ class SummaryMailer < ApplicationMailer
 
     attending = []
     not_attending = []
+    has_not_responded = []
 
     rsvp_array.each do |person|
-      if person.is_attending
-        attending << "#{person.full_name} - Updated at: #{person.updated_at}"
+      if person.email
+        if person.is_attending
+          attending << "#{person.full_name} - Updated at: #{person.updated_at}"
+        else
+          not_attending << "#{person.full_name} - Updated at: #{person.updated_at}"
+        end
       else
-        not_attending << "#{person.full_name} - Updated at: #{person.updated_at}"
+        has_not_responded << person.full_name
       end
     end
 
     {
       attending: attending,
-      not_attending: not_attending
+      not_attending: not_attending,
+      has_not_responded: has_not_responded
     }
   end
 end
