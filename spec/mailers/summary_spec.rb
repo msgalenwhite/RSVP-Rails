5fa10848368bcc959ff6ccee7b1be87043209885
Galen <galen.h.white@gmail.com>
2018-07-23 14:56:23 -0700

    reverted

--- spec/mailers/summary_spec.rb
+++ spec/mailers/summary_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 describe SummaryMailer, :type => :mailer do
-  let(:send_email) { 'galen.h.white@gmail.com' }
-
   before (:each) do
     SummaryMailer.send_out('test@test.com').deliver_now
     @test_mail = ActionMailer::Base.deliveries[0]
@@ -15,7 +13,7 @@ describe SummaryMailer, :type => :mailer do
     it 'renders an email with the correct headers' do
       expect(@test_mail.subject).to eq("Your Summary Email")
       expect(@test_mail.to).to eq(['test@test.com'])
-      expect(@test_mail.from).to eq([send_email])
+      expect(@test_mail.from).to eq([ENV['GALEN_EMAIL']])
     end
     it 'renders an email with the correct body' do
       expect(@test_mail.body).to match('Here is your weekly update email:')
