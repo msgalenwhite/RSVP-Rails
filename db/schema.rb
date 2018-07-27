b264208c48122245947021b0266fd982ef1a9685
Galen <galen.h.white@gmail.com>
2018-07-27 08:37:17 -0700

    added stories and cleaned up user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_220219) do
+ActiveRecord::Schema.define(version: 2018_07_27_153603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,9 +31,15 @@ ActiveRecord::Schema.define(version: 2018_07_11_220219) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "invite_id", null: false
-    t.integer "user_id"
     t.string "role", default: "guest", null: false
     t.text "email"
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.string "submitter", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
