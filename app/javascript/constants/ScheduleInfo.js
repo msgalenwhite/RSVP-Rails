658bab1104bcbb4fc91d984f816542c4a66b30df
Galen <galen.h.white@gmail.com>
2018-10-09 07:38:45 -0400

    updated schedule

---
+++ app/javascript/constants/ScheduleInfo.js
@@ -0,0 +1,20 @@
+const scheduleInfo = {
+  "Friday, October 12th": [
+    "11:30 am Apple Picking at Russell Orchards",
+    "1:00 pm Lawn Games",
+    "4:00 pm Rehearsal (Bridal Party Only)",
+    "5:00 pm Off-Site Dinner with Galen and Chris (Bridal Party Only)"
+  ],
+  "Saturday, October 13th": [
+    "1:30 pm Family Photos",
+    "3:30 pm Ceremony",
+    "4:00 pm Wandering Cocktail Hour",
+    "5:00 pm Toasts, Dinner, Dancing, and Cake!",
+    "10:00 pm Last Song"
+  ],
+  "Sunday, October 14th": [
+    "9:00 am Brunch (by RSVP only)",
+    "11:00 am Room Checkout"
+  ]
+}
+export default scheduleInfo
